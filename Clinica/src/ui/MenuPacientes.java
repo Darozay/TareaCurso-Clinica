48929359b3ac6acf037626084f4226490ee72311
Alcira <salinasalcira@hotmail.com>
2022-09-29 13:06:09 -0600

    Subiendo cambios

--- Clinica/src/ui/MenuPacientes.java
+++ Clinica/src/ui/MenuPacientes.java
@@ -12,7 +12,7 @@ public class MenuPacientes {
     }
     
     public void Menu() {
-        System.out.println("_________________________________________________________________________________");
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         System.out.println("1.Buscar paciente en especifico");
         System.out.println("2.Buscar pacientes");
         System.out.println("3.Volver");
